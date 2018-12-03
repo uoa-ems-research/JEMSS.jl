2331655f693264e4fa5d75e600ff8538e8a47e42
Samuel Ridler <samridler@hotmail.com>
2018-12-03 15:10:31 +1300

    License boilerplates

--- example/scripts/deployment_local_search.jl
+++ example/scripts/deployment_local_search.jl
@@ -1,3 +1,18 @@
+##########################################################################
+# Copyright 2017 Samuel Ridler.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+
 # Run a local search algorithm to optimise ambulance deployment, given some objective function (objFn).
 # The local search is simple hill climbing, and the neighbourhood of a solution
 # (deployment) involves moving one ambulance from its station to another station.
