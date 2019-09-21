0165d1d58195180a834fc5f5ae7789da4cc48cde
Samuel Ridler <samridler@hotmail.com>
2019-09-21 19:40:03 +1200

    Change `deepcopy` instance
    `copy` is sufficient (and faster).

--- src/decision/move_up/zhang_ip.jl
+++ src/decision/move_up/zhang_ip.jl
@@ -82,7 +82,7 @@ function zhangIpMoveUp(sim::Simulation)::Tuple{Vector{Ambulance}, Vector{Station
 	# for at-station or newly-freed ambulances: adjusted time = original time
 	# for on-road ambulances: adjusted time = original time * ( time spent on route + time to drive to station - time from route origin to station <= regret-travel-time threshold ? discount factor : 1 )
 	# for at-hospital ambulances: adjusted time = original time + expected remaining handover duration
-	adjustedAmbToStationTimes = deepcopy(ambToStationTimes)
+	adjustedAmbToStationTimes = copy(ambToStationTimes)
 	travelMode = getTravelMode!(sim.travel, lowPriority, sim.time)
 	for i = 1:numMoveUpAmbs
 		ambulance = moveUpAmbs[i]
