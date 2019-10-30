0d5cecd211dad4941f7beeec2b4b650d694f84f9
Samuel Ridler <samridler@hotmail.com>
2019-10-30 13:05:26 +1300

    transient.jl license

--- example/scripts/transient.jl
+++ example/scripts/transient.jl
@@ -1,3 +1,18 @@
+##########################################################################
+# Copyright 2017 Samuel Ridler.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+
 # For determining transient period duration at start and end of simulation, based on call response durations.
 # Useful for deciding how duration of simulation warm-up and cool-down periods.
 # Loads sim from config, splits calls into batches, runs simulation for each batch,
