89359e9105c349c779a92841db8b416285566ae0
Samuel Ridler <samridler@hotmail.com>
2020-01-07 19:21:12 +1300

    Zip unpack test revert

--- data/temp/unzip_data.jl
+++
@@ -1,7 +0,0 @@
-using BinaryProvider
-cd(@__DIR__) do
-	@info("Testing BinaryProvider.unpack().")
-	unpack("temp.gz", pwd())
-	@assert(isfile("file 1.txt"))
-	@assert(isfile("file 2.txt"))
-end
--- deps/build.jl
+++ deps/build.jl
@@ -1,3 +1,1 @@
-# include("../data/unzip_data.jl")
-@warn("JEMSS build script has temporary changes for testing purposes.")
-include("../data/temp/unzip_data.jl")
+include("../data/unzip_data.jl")
