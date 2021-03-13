d729f6e90e754b280dc0ca671a37e3359883888e
Samuel Ridler <samridler@hotmail.com>
2021-03-13 15:03:30 +1300

    Install selected packages

--- deps/build.jl
+++ deps/build.jl
@@ -1,1 +1,9 @@
+using Pkg
+
+# Install packages which JEMSS needs to know the version numbers of,
+# can only seem to get version numbers for installed packages, using `Pkg.installed()`
+Pkg.add("ArchGDAL")
+Pkg.add("JuMP")
+Pkg.add("LightGraphs")
+
 include("../data/unzip_data.jl")
